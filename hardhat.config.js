96c643ad3fe99c62f7dbfdf5bd86bb2fdc2b8cfc
TamilarasiSparkout <tamilarasi@sparkouttech.com>
2025-05-08 17:36:55 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,23 +4,40 @@ require("dotenv").config();
 require("@openzeppelin/hardhat-upgrades");
 
 
-const { HOLESKY_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+const {BSC_RPC_URL, MONAD_RPC_URL, HOLESKY_RPC_URL, PRIVATE_KEY, BSC_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    holesky: {
-      url: HOLESKY_RPC_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
+   // holesky: {
+   //   url: HOLESKY_RPC_URL,
+   //   accounts: [`0x${PRIVATE_KEY}`],
+   // },
+   //  monad: {
+   //    url: MONAD_RPC_URL,
+   //    accounts: [`0x${PRIVATE_KEY}`],
+   // },
+     bscTestnet: {
+       url: BSC_RPC_URL,
+       accounts: [`0x${PRIVATE_KEY}`],
+     }
+     
   },
   sourcify: {
     enabled: true,
   },
+
   etherscan: {
+    //apiKey: {
+      //holesky: ETHERSCAN_API_KEY,
+    //},
+    //apiKey: {
+    //  monad: ETHERSCAN_API_KEY,
+    //},
     apiKey: {
-      holesky: ETHERSCAN_API_KEY,
-    },
+      bscTestnet: BSC_API_KEY,
+    }
   },
 };
+
